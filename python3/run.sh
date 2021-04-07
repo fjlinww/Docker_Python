1738123dbd73075d06ea7a8666067a8c532be893
fjlinww <williamlin03@vip.qq.com>
2021-04-07 17:17:46 +0800

    20210407

--- python3/run.sh
+++ python3/run.sh
@@ -1,4 +1,3 @@
 docker run -it \
 	-v $PWD/apps:/usr/src/app  \
-        --device=/dev/video0 \
-        --rm  wdf000/python3:20210407 face_distance.py
+        --rm  wdf000/python3 face_distance.py

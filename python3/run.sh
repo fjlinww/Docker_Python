98e3aecc34394db3b2c73759deb8d0f34df88dd9
fjlinww <williamlin03@vip.qq.com>
2021-04-07 16:59:37 +0800

    add facerec app

--- python3/run.sh
+++ python3/run.sh
@@ -1,1 +1,4 @@
-docker run -it -v $PWD/apps:/usr/src/app  --rm  wdf000/python3 testopencv.py
+docker run -it \
+	-v $PWD/apps:/usr/src/app  \
+        --device=/dev/video0 \
+        --rm  wdf000/python3:20210407 face_distance.py
